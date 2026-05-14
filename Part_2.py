6c2eb2e5b1339b1b1dce57500d92ad7325c6d099
Felicia <nkosifelician8@gmail.com>
2026-05-14 15:00:57 +0200

    Update Part_2.py

--- Part_2.py
+++ Part_2.py
@@ -6,8 +6,8 @@ import os
 # FAST DOWNWARD PATH
 # =========================================
 FAST_DOWNWARD = "fast-downward.py"
-DOMAIN = "domain_mod.pddl"
-COMPLIANT = "problem_compliant.pddl"
+DOMAIN = "domain.pddl"
+COMPLIANT = "problem.pddl"
 NONCOMPLIANT = "problem_noncompliant.pddl"
 
 # =========================================
